d2c39d2925a8cb07cdf08b377221f8c82c983031
mgapsa <gapsamikolaj@gmail.com>
2018-01-07 13:41:14 +0100

    Algorithm fixes - nows houses are replaced properly by gas stations

--- Solver.hs
+++ Solver.hs
@@ -10,11 +10,11 @@ module Solver where
     | r < (length rows -1) && c > (length cols -1) = solveImpl map (r+1, 0) rows cols
     | (getFieldTypeAt map (r,c)) == House && c >= (length cols -1) = solveImpl (putGasNextToHouse map (r, c) rows cols) (r+1, 0) rows cols
     | (getFieldTypeAt map (r,c)) == House && r >= (length rows -1) && c >= (length cols -1) = putGasNextToHouse map (r, c) rows cols
-    | (getFieldTypeAt map (r,c)) == House = solveImpl (putGasNextToHouse map (r, c+1) rows cols) (r, c+1) rows cols
+    | (getFieldTypeAt map (r,c)) == House = solveImpl (putGasNextToHouse map (r, c) rows cols) (r, c+1) rows cols
     | r >= (length rows -1) && c >= (length cols -1) = map
     | c >= (length cols -1) = solveImpl map (r+1, 0) rows cols
     | otherwise = solveImpl map (r, c+1) rows cols
 
   --dodac sprawdzanie w jednej linii i jakies returny jak slepy zaulek
   putGasNextToHouse :: Map -> Point -> [Int] -> [Int] -> Map
-  putGasNextToHouse map (r, c) rows cols = putFieldAt map (r, c) Gas rows cols
+  putGasNextToHouse map (r, c) rows cols = putFieldAt map (c, r) Gas rows cols
