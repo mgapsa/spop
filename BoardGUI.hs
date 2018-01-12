76ec0b3b1ed89c47be177408774f5925e1126585
mgapsa <gapsamikolaj@gmail.com>
2018-01-12 15:24:22 +0100

    ELO

--- BoardGUI.hs
+++ BoardGUI.hs
@@ -14,7 +14,7 @@ module BoardGUI where
     putStr " "
     printElements (cols board)
     putStr "\n"
-    drawMapImpl map (cols board)
+    drawMapImpl map (rows board)
 
   drawMapImpl :: Map -> [Int] -> IO ()
   drawMapImpl [] _ = putStr "\n"
